868209b74b21a7968f5bd1f4632deb8061285723
s.lukianenko <lukyanenko88@gmail.com>
2022-02-13 18:50:34 +0200

    Initial commit

---
+++ src/main/java/com/deo/kafkajet/KafkaJetApplication.java
@@ -0,0 +1,13 @@
+package com.deo.kafkajet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaJetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaJetApplication.class, args);
+    }
+
+}
